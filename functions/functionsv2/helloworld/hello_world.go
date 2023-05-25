5148acb581a768105518aa35e138e6d3162b2d6e
sharad.gupta <sharad.gupta@missionlane.com>
2023-05-24 22:15:31 -0700

    Write to http Response

--- functions/functionsv2/helloworld/hello_world.go
+++ functions/functionsv2/helloworld/hello_world.go
@@ -18,7 +18,6 @@
 package helloworld
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
@@ -30,7 +29,7 @@ func init() {
 
 // helloGet is an HTTP Cloud Function.
 func helloGet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World!")
+	w.Write([]byte("Hello, World!"))
 }
 
 // [END functions_helloworld_get]
